cca091d0f3bfcbba5cb2833d466472436d12f2ff
GochoMugo <mugo@forfuture.co.ke>
2016-09-28 10:39:07 +0300

    save 1

---
+++ bindings/node/binding.gyp
@@ -0,0 +1,5 @@
+{
+  "include_dirs" : [
+    "<!(node -e \"require('nan')\")"
+   ]
+}
---
+++ bindings/node/index.js
@@ -0,0 +1,11 @@
+/**
+ * Node.js bindings for the contra library.
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2016 GochoMugo <mugo@forfuture.co.ke>
+ * Copyright (c) 2016 Forfuture, LLC <we@forfuture.co.ke>
+ */
+
+
+// npm-installed modules
+const bindings = require("bindings");

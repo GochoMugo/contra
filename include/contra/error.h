d67d58840f061d381e34a6475fec483d1eed2254
GochoMugo <mugo@forfuture.co.ke>
2024-08-23 04:24:36 +0300

    doc: document errors

--- include/contra/error.h
+++ include/contra/error.h
@@ -8,13 +8,14 @@
 #ifndef _CONTRA_error_h_
 #define _CONTRA_error_h_ 1
 
-
 /** Errors */
 typedef enum {
-    /* General errors */
-    CONTRA_ERR = -1000,
-    CONTRA_ERR_MALLOC,
-    CONTRA_ERR_BAD_ARGS,
+  /* General errors */
+  CONTRA_ERR = -1000,
+  /** Could not allocate memory. */
+  CONTRA_ERR_MALLOC,
+  /** Bad arguments were provided. */
+  CONTRA_ERR_BAD_ARGS,
 } contra_error_code;
 
 #endif
